4ad8d98b41f4d14c1d2c85fe2322b16d44a84cc1
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-02 00:57:09 -0500

    stdin, quit on ctrl +C

--- play.js
+++ play.js
@@ -1,6 +1,29 @@
 const net = require("net");
 const connect = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+
+const handleUserInput = function(data) {
+  // your code here
+  // \u0003 maps to ctrl+c input
+  if (data === '\u0003') {
+    process.exit();
+  }
+
+};
 
 
 console.log("Connecting ...");
