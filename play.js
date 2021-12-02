4da10b710929e8c90623cebfb9d74c31e34c6117
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-12-01 22:56:58 -0500

    connection separate the connection function in client.js

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: "50541"// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-    // connection.write(`🦜: ${data}`);
-    // broadcast(listOfConnections, data);
-
-    // Send a message to everybody but the sender
-    //	betterBroadcast(listOfConnections, data, connection.id);
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
